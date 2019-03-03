1092a19a6e97871ee81f5279e269a18c866f6208
ahouzi <mr.ahouzi@gmail.com>
2019-03-03 22:34:15 +0100

    day one : working with props

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Faker from "faker";
+import CommentDetail from './CommentDetail';
+import ApprovalCard from "./ApprovalCard";
 
 
-function getButtonText() {
 
-    return 'Click on me!';
-}
 
 
 // create react component
@@ -15,12 +15,18 @@ function getButtonText() {
 const App =  () => {
 
     return (
-        <div>
-            <label className="label" htmlFor="name"> Enter name : </label>
-            <input id="name" type="text"/>
-            <button style={{backgroundColor:'blue', color:'white'}} > {getButtonText()}</button>
+      <div className="ui container comments">
+          <ApprovalCard>
+          <CommentDetail avt={Faker.image.avatar()} author="Sam" timeAgo="Today at 6:OO Am" description="Nice JOB!"/>
+          </ApprovalCard>
+          <ApprovalCard>
+          <CommentDetail avt={Faker.image.avatar()} author="Oga" timeAgo="Today at 9:OO Am" description="Nice Blog!"/>
+          </ApprovalCard>
+          <ApprovalCard>
+          <CommentDetail avt={Faker.image.avatar()} author ="Yeo" timeAgo="Today at 12:OO Am" description="I like you!"/>
+          </ApprovalCard>
+      </div>
 
-        </div>
     );
 };
 
